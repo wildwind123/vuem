20bd63ae4c83fe5464051dc9090465021d43454b
Petr <vinokurovvin@list.ru>
2025-01-15 08:41:24 +0900

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    {{ props.label }}
+  </div>
+</template>
+<script lang="ts" setup>
+const props = defineProps<{
+  label: string
+}>()
+</script>
