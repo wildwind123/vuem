f295259e8043aa8b0032dc3413bd5f8efdd368d5
Petr <vinokurovvin@list.ru>
2025-01-15 12:07:05 +0900

    CategoryList

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -8,46 +8,21 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
           </svg>
         </div>
-        <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-          <li><a>Item 1 asdf</a></li>
-          <li>
-            <a>Parent</a>
-            <ul class="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </li>
-          <li><a>Item 3</a></li>
-        </ul>
-
+        <CategoryList :category-list="props.categoryList"></CategoryList>
       </div>
       <slot name="name">
         <a class="btn btn-ghost text-xl">daisyUI</a>
       </slot>
     </div>
     <div class="navbar-center hidden lg:flex">
-      <ul class="menu menu-horizontal px-1">
-        <li><a>Item 1</a></li>
-        <li>
-          <details>
-            <summary>Parent</summary>
-            <ul class="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </details>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
       <CategoryList :category-list="props.categoryList" :is-mobile="false"></CategoryList>
     </div>
     <div class="navbar-end">
       <slot name="end">
-        <!-- <a class="btn">Button</a> -->
       </slot>
     </div>
   </div>
-  <CategoryList :category-list="props.categoryList"></CategoryList>
+
 </template>
 <script lang="ts" setup>
 import CategoryList from '#vuem/CategoryList.vue';
