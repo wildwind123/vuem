21469f7caa8c13e24f2870ee55b41012d75b4225
Petr <vinokurovvin@list.ru>
2025-01-16 22:12:44 +0900

    flex-wrap

--- src/components/input/SelectedItems.vue
+++ src/components/input/SelectedItems.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex gap-4">
+  <div class="flex gap-4 flex-wrap">
     <div v-for="item in props.items" class="badge" :class="item.class ? item.class : 'bg-accent'" :key="item.id">{{
       item.value }}<button @click="emit('event:close', item)" class="ml-4">
         <IconClose></IconClose>
