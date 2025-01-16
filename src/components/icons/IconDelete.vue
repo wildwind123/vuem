4a2456b1aaa50c1ecd7bc8bc6c3278eded70bfed
Petr <vinokurovvin@list.ru>
2025-01-16 15:19:50 +0900

    IconDelete

---
+++ src/components/icons/IconDelete.vue
@@ -0,0 +1,6 @@
+<template>
+  <Icon icon="material-symbols:delete-forever-sharp" width="16" height="16" />
+</template>
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+</script>
--- src/components/icons/group.ts
+++ src/components/icons/group.ts
@@ -1,7 +1,9 @@
 import IconEditSquareVue from '#vuem/icons/IconEditSquare.vue'
+import IconDelete from '#vuem/icons/IconDelete.vue'
 
 const Icons = {
   EditSquare: IconEditSquareVue,
+  Delete: IconDelete,
 }
 
 export { Icons }
