b04534fb67e3f2f95826f7ae9f5101718154f7cb
Petr <vinokurovvin@list.ru>
2025-02-01 15:18:33 +0900

    checkPhone

--- src/components/input/InputPhone.vue
+++ src/components/input/InputPhone.vue
@@ -1,29 +1,47 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="form-control w-full ">
-    <div v-if="props.label" class="label">
-      <span class="label-text font-semibold">{{ props.label }}</span>
+  <div>
+    <div class="flex items-center gap-1">
+      <select v-model="cValue" class="select select-bordered select-sm  ">
+        <option :value="item[0]" class="text-end" v-for="(item, i) in countryInfoMap" :key="i">
+          {{ item[0] }} {{ item[1].icon }} +{{ item[1].prefix }}
+        </option>
+      </select>
+      <input v-model="value" @keydown="checkDigit" type="text" class="input input-bordered input-sm">
+      <div v-if="!props.allowEmpty" class="inline text-red-500">*</div>
     </div>
-    <div class="flex items-center">
-      <input :value="value" @change="value = ($event.target as HTMLInputElement).value" type="text"
-        :placeholder="props.placeholder" class="input input-bordered w-full  input-sm mr-2" />
-      <div v-if="meta.required" class="inline text-red-500">*</div>
-    </div>
-    <div v-if="errorMessage" class="label">
-      <span class="label-text-alt text-red-500">{{ errorMessage }}</span>
+    <div>
+      <div v-if="errorMessage || cErrorMessage" class="label">
+        <span class="label-text-alt text-red-500">{{ errorMessage }}. {{ cErrorMessage }}</span>
+      </div>
     </div>
   </div>
 </template>
-
-<script setup lang="ts">
+<script lang="ts" setup>
+import { getCountryInfoMap } from '#src/vuem/src/components/xutils/country_map.ts';
 import { useField } from 'vee-validate';
 
+
 const props = defineProps<{
-  name: string,
-  label?: string,
-  placeholder?: string,
+  phoneName: string,
+  countryCodeName: string,
+  allowEmpty?: boolean
 }>()
 
-const { value, errorMessage, meta } = useField(() => props.name)
+const checkDigit = (event: KeyboardEvent) => {
+  const check = event.key.length === 1 && isNaN(Number(event.key));
+  const isShortcut = event.metaKey || event.ctrlKey;
+  if (check && !isShortcut) {
+    event.preventDefault();
+  }
+}
+
+const countryInfoMap = getCountryInfoMap()
+
+
+
+const { value, errorMessage } = useField(() => props.phoneName)
+const { value: cValue, errorMessage: cErrorMessage, } = useField(() => props.countryCodeName)
 
 
 </script>
--- src/components/xutils/country_map.ts
+++ src/components/xutils/country_map.ts
@@ -1,14 +1,21 @@
-import { getCountries, getCountryCallingCode } from 'libphonenumber-js'
+import {
+  getCountries,
+  getCountryCallingCode,
+  parsePhoneNumberWithError,
+  type CountryCode,
+} from 'libphonenumber-js'
 import getUnicodeFlagIcon from 'country-flag-icons/unicode'
+import zod from 'zod'
+
+type CountryInfo = {
+  code: CountryCode
+  prefix: string
+  icon: string
+}
 
 const getCountryInfoMap = () => {
   const countries = getCountries()
   const countryCodeInfoMap = new Map<string, CountryInfo>()
-  type CountryInfo = {
-    code: string
-    prefix: string
-    icon: string
-  }
 
   for (let i = 0; i < countries.length; i++) {
     const countryInfo = {
@@ -33,4 +40,57 @@ const getCountryInfoMap = () => {
   return countryCodeInfoMap
 }
 
-export { getCountryInfoMap }
+const checkPhone = (phone: string, info: CountryInfo) => {
+  try {
+    const phoneNumber = parsePhoneNumberWithError(phone, {
+      defaultCountry: info.code as CountryCode,
+    })
+    const valid = phoneNumber?.isValid()
+    return valid
+  } catch {
+    return false
+  }
+}
+
+const getZodPhoneCheck = (allowEmpty: boolean) => {
+  let numberCheck = zod.string()
+  if (allowEmpty) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    numberCheck = zod.string().optional()
+  }
+
+  return zod
+    .object({
+      number: numberCheck,
+      country_code: zod.string().min(2),
+    })
+    .superRefine((v, ctx) => {
+      if (allowEmpty && (v.number == undefined || v.number == '')) {
+        return true
+      } else if ((!allowEmpty && v.number == undefined) || v.number == '') {
+        return false
+      }
+
+      let valid = false
+      try {
+        const phoneNumber = parsePhoneNumberWithError(v.number!, {
+          defaultCountry: v.country_code as CountryCode,
+        })
+        valid = phoneNumber?.isValid()
+      } catch {
+        valid = false
+      }
+
+      if (valid == true) {
+        return true
+      }
+      ctx.addIssue({
+        code: zod.ZodIssueCode.custom,
+        path: ['number'],
+        message: 'Проверьте номер телефона',
+      })
+    })
+}
+
+export { getCountryInfoMap, checkPhone, getZodPhoneCheck }
