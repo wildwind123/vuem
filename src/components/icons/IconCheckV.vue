35c0c1fe29ffb1545043fbc4803747a53fc2137c
Petr <vinokurovvin@list.ru>
2025-02-04 22:59:02 +0900

    IconCheckV

---
+++ src/components/icons/IconCheckV.vue
@@ -0,0 +1,6 @@
+<template>
+  <Icon icon="material-symbols:check-circle-outline" width="16" height="16" />
+</template>
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+</script>
--- src/components/icons/group.ts
+++ src/components/icons/group.ts
@@ -1,11 +1,13 @@
 import IconEditSquareVue from './IconEditSquare.vue'
 import IconDelete from './IconDelete.vue'
 import IconClose from './IconClose.vue'
+import IconCheckV from './IconCheckV.vue'
 
 const Icons = {
   EditSquare: IconEditSquareVue,
   Delete: IconDelete,
   Close: IconClose,
+  IconCheckV: IconCheckV,
 }
 
 export { Icons }
