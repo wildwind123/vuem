d81ce580265f8a07ef2887309fc70c2d268c2640
Petr <vinokurovvin@list.ru>
2025-02-05 22:00:06 +0900

    EditSquareTable

--- src/components/icons/group.ts
+++ src/components/icons/group.ts
@@ -2,12 +2,14 @@ import IconEditSquareVue from './IconEditSquare.vue'
 import IconDelete from './IconDelete.vue'
 import IconClose from './IconClose.vue'
 import IconCheckV from './IconCheckV.vue'
+import IconEditSquareTable from './IconEditSquareTable.vue'
 
 const Icons = {
   EditSquare: IconEditSquareVue,
   Delete: IconDelete,
   Close: IconClose,
   IconCheckV: IconCheckV,
+  EditSquareTable: IconEditSquareTable,
 }
 
 export { Icons }
