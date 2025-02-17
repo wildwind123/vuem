d0350fe3cf453a76ea1f62fe53fefbeded6f0f34
Petr <vinokurovvin@list.ru>
2025-02-17 17:59:48 +0900

    dayjs

--- src/components/xutils/daysjs.ts
+++ src/components/xutils/daysjs.ts
@@ -1,9 +1,10 @@
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 import timezone from 'dayjs/plugin/timezone'
-
+import 'dayjs/locale/ru'
 dayjs.extend(utc)
 dayjs.extend(timezone)
+dayjs.locale('ru')
 
 export { dayjs as dayjsTz }
 
